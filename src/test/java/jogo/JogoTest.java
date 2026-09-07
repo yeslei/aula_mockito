943c0c6160b02b89e5af5d78aa9537d61f9de61f
Weslley Ribeiro <weslleysilvaribeiro35@gmail.com>
2026-09-07 15:51:44 -0300

    mockado

--- src/test/java/jogo/JogoTest.java
+++ src/test/java/jogo/JogoTest.java
@@ -1,6 +1,25 @@
 package jogo;
 
-public class JogoTest {
-	
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.Test;
+
+public class JogoTest {
+
+    @Test
+    public void testarJogo() {
+
+        Jogador jogador = mock(Jogador.class);
+        Dado dado1 = mock(Dado.class);
+        Dado dado2 = mock(Dado.class);
+
+        when(jogador.lancar(dado1, dado2)).thenReturn(7);
+
+        Jogo jogo = new Jogo();
+
+        boolean resultado = jogo.jogo();
+
+        assertTrue(resultado);
+    }
 }
